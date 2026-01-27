dd2f9e9a138a430f9cf0177af7a83ef8165e8e5f
brunamenezes1 <brunamenezes170920@gmail.com>
2026-01-27 16:01:27 -0300

    mudando o formato das subclasses

---
+++ cacador.java
@@ -0,0 +1,29 @@
+class Cacador extends Soldado {
+//terceira subclasse de Soldado (Caçador)
+    private Random random = new Random();
+
+    public Cacador(String nome, int nivel) {
+        super(nome, nivel, 70);
+    }
+//método de ataque com chance de acerto crítico
+    @Override
+    public void atacar(Soldado inimigo) {
+        int dano = 20 + nivel * 2;
+        boolean critico = random.nextInt(100) < 25;
+
+        if (critico) {
+            dano *= 2;
+            System.out.println(nome + " acertou um ATAQUE CRÍTICO!");
+        } else {
+            System.out.println(nome + " disparou uma flecha!");
+        }
+
+        inimigo.defender(dano);
+    }
+//método de defesa simples
+    @Override
+    public void defender(int danoRecebido) {
+        dano(danoRecebido);
+        System.out.println(nome + " recebeu " + danoRecebido + " de dano.");
+    }
+}
