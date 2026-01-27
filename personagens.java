b2170b62f5bcb4867e25e515e6d08e217175e521
brunamenezes1 <brunamenezes170920@gmail.com>
2026-01-27 15:14:56 -0300

    adicionando arcanista

--- personagens.java
+++ personagens.java
@@ -57,4 +57,34 @@ class GuardiaoDeFerro extends Soldado {
         }
     }
 }
+class Arcanista extends Soldado {
+
+    private int mana;
+
+    public Arcanista(String nome, int nivel) {
+        super(nome, nivel, 80);
+        this.mana = 100;
+    }
+
+    @Override
+    public void atacar(Soldado inimigo) {
+        if (mana >= 20) {
+            int dano = 30 + nivel * 3;
+            mana -= 20;
+            System.out.println(nome + " lança um feitiço!");
+            inimigo.defender(dano);
+        } else {
+            int dano = 8;
+            mana += 10;
+            System.out.println(nome + " ataca fisicamente e recupera mana!");
+            inimigo.defender(dano);
+        }
+    }
+
+    @Override
+    public void defender(int danoRecebido) {
+        dano(danoRecebido);
+        System.out.println(nome + " sofreu " + danoRecebido + " de dano.");
+    }
+}
 
